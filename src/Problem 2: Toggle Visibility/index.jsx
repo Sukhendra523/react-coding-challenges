d2289be468b2d3cea0b58d621ecffc11f130770d
sukhendra523 <523sukh@gmail.com>
2024-07-17 01:27:12 +0530

    Solved Problem 2: Toggle Visibility

---
+++ src/Problem 2: Toggle Visibility/index.jsx
@@ -0,0 +1,40 @@
+/* 
+
+    - Create a button that toggles the visibility of a paragraph.
+    - Initially, the paragraph should be visible.
+    - When the button is clicked, the paragraph should be hidden, and the button text should change to "Show".
+    - When the button is clicked again, the paragraph should be shown, and the button text should change to "Hide".
+
+ */
+
+import { useState } from "react";
+
+//1st
+// const VisibilityToggle = () => {
+//   const [showParagraph, setShowParagraph] = useState(true);
+
+//   const toggleVisiblity = () => setShowParagraph(!showParagraph);
+//   return (
+//     <div>
+//       {showParagraph && <p>Paragraph text</p>}
+//       <button onClick={toggleVisiblity}>
+//         {showParagraph ? "Hode" : "Show"}
+//       </button>
+//     </div>
+//   );
+// };
+
+//2nd scalable
+const VisibilityToggle = ({ children }) => {
+  const [show, setShow] = useState(true);
+
+  const toggleVisiblity = () => setShow(!show);
+  return (
+    <div>
+      <button onClick={toggleVisiblity}>{show ? "Hide" : "Show"}</button>
+      {show && children}
+    </div>
+  );
+};
+
+export default VisibilityToggle;
