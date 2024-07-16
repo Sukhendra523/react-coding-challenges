36daccdbb477050d97778ade891759ed166f9587
sukhendra523 <523sukh@gmail.com>
2024-07-17 01:28:36 +0530

    Solved Problem 3: Fetch and Display Data

---
+++ src/Problem 3: Fetch and Display Data/Hook/useFetchData.jsx
@@ -0,0 +1,37 @@
+import { useCallback, useEffect, useState } from "react";
+const defaultOptions = { method: "GET" };
+
+const useFetchData = (url, initialData, options = defaultOptions) => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [data, setData] = useState(initialData);
+
+  const fetchData = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url, options);
+
+      if (response.ok) {
+        const responseData = await response.json();
+        setData(responseData);
+      }
+    } catch (error) {
+      setError(
+        error.message ||
+          error.body.message ||
+          "Sorry something went wrong please again later!"
+      );
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }, [url, options]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return { loading, data, error };
+};
+
+export default useFetchData;
---
+++ src/Problem 3: Fetch and Display Data/index.jsx
@@ -0,0 +1,36 @@
+/* 
+
+- Create a button that, when clicked, fetches data from an API and displays it in a list.
+- Use a placeholder API like [JSONPlaceholder](https://jsonplaceholder.typicode.com/).
+- Display a loading message while fetching the data and handle any potential errors gracefully.
+
+*/
+
+// PostContainer
+// States = > posts , loading , error
+// fetch post on mount
+
+import useFetchData from "./Hook/useFetchData";
+
+const PostContainer = () => {
+  const {
+    loading,
+    data: posts,
+    error,
+  } = useFetchData("https://jsonplaceholder.typicode.com/posts", []);
+
+  return (
+    <div>
+      {loading && <span>Loading....</span>}
+      {posts.map(({ title, body, id }) => (
+        <div key={id}>
+          <h1>{title}</h1>
+          <p>{body}</p>
+        </div>
+      ))}
+      {error && !loading && <p>{error}</p>}
+    </div>
+  );
+};
+
+export default PostContainer;
